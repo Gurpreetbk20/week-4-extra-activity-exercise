ca5bcd005d69ff78c5bd60dd84d6174a38bcea28
Gurpreetbk20 <160788997+Gurpreetbk20@users.noreply.github.com>
2024-03-15 12:45:51 +1300

    Add files via upload

---
+++ week 4 extra activity exercise.py
@@ -0,0 +1,29 @@
+x=23 
+y=10
+z=0
+print(x)
+print(y) 
+print(z)
+
+
+# Assigning multiple variables in a single statement
+a, b, c = 1, 2, 3
+
+print("a =", a)
+print("b =", b)
+print("c =", c)
+
+# Tuple unpacking
+x, y, z = (4, 5, 6)
+
+print("x =", x)
+print("y =", y)
+print("z =", z)
+
+# Swapping variables
+x = 10
+y = 20
+x, y = y, x
+
+print("x =", x)
+print("y =", y)
